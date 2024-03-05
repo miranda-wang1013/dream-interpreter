62e3b91680e207846559a0ce4deb899bc84313e8
miranda-wang1013 <miranda_wang@foxmail.com>
2024-03-06 00:49:40 +0800

    Deployment

--- src/components/Dream.jsx
+++ src/components/Dream.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
 
 export default function Dream({ getImage, getGptResponse, imageUrl, gptResponse, setImageUrl, savedItems, setSavedItems, deleteItem }) {
     const [topText, setTopText] = React.useState('');
     const [bottomText, setBottomText] = React.useState('');
+    const [defaultText, setDefaultText] = React.useState('Default Text');
+    const [defaultImageUrl, setDefaultImageUrl] = React.useState('default-image.jpg');
 
-    // 定义解梦方法数组
+    useEffect(() => {
+        setDefaultText('Dreams can provide valuable insights into our subconscious thoughts, emotions, and desires.');
+        setDefaultImageUrl('https://images.unsplash.com/photo-1709486511766-76bdd8b51713?q=80&w=3189&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+    }, []);
+
+   
     const interpretationMethods = [
         { value: 'empty', label: "Choose one interpretation methods" },
         { value: 'freud', label: "Freud's Psychoanalytic Approach(弗洛伊德)" },
@@ -67,10 +75,12 @@ export default function Dream({ getImage, getGptResponse, imageUrl, gptResponse,
             </form>
             <div className="form--output-container">
                 <div className="form--output-text">
-                    {gptResponse && <div dangerouslySetInnerHTML={{ __html: formatResponse(gptResponse) }} />}
+                {!gptResponse && <div dangerouslySetInnerHTML={{ __html: defaultText }} />}
+                {gptResponse && <div dangerouslySetInnerHTML={{ __html: formatResponse(gptResponse) }} />}
                 </div>
                 <div className="form--output-image">
-                    {imageUrl && <img src={imageUrl} alt="Dream Image"/>}
+                {!imageUrl && <img src={defaultImageUrl} alt="Default Image" style={{ maxWidth: '250px', maxHeight: '250px' }} />}
+                {imageUrl && <img src={imageUrl} alt="Dream Image" style={{ maxWidth: '250px', maxHeight: '250px' }} />}
                 </div>
             </div>
             <div className="Dream--storage">
