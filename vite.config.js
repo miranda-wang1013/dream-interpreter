925cd65b5e5850ee775683f574ff607488600189
miranda-wang1013 <miranda_wang@foxmail.com>
2024-03-06 22:35:45 +0800

    Add workflow to use OpenAI API Key

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://miranda-wang1013.github.io/dream-interpreter/', 
   plugins: [react()],
 })
